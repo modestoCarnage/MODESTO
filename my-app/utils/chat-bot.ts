10ff0c18ed85bdbc3d4db0fade38f64b6b2ed9ea
Justin <delarocaaaron@gmail.com>
2024-08-05 21:26:28 +0800

    Edited

--- my-app/utils/chat-bot.ts
+++ my-app/utils/chat-bot.ts
@@ -1,36 +1,38 @@
 export const userMessage = [
-  ["hi", "hey", "hello", "greetings"],
-  ["sure", "yes", "no"],
-  ["are you genious", "are you nerd", "are you intelligent"],
-  ["i hate you", "i dont like you"],
-  ["how are you", "how is life", "how are things", "how are you doing"],
-  ["what are you doing", "what is going on", "what is up"],
-  ["how old are you"],
-  ["who are you", "are you human", "are you bot", "are you human or bot"],
-  ["who created you", "who made you", "who is your creator"],
+  ["hi", "hey", "hello", "greetings"], //1
+  ["sure", "yes", "no"], //2
+  ["are you genius", "are you nerd", "are you intelligent"], //3
+  ["i hate you", "i dont like you"], //4
+  ["how are you", "how is life", "how are things", "how are you doing"], //5
+  ["what are you doing", "what is going on", "what is up"], //6
+  ["how old are you"], //7
+  ["who are you", "are you human", "are you bot", "are you human or bot"], //8
+  ["who created you", "who made you", "who is your creator"], //9
 
   [
+    //10
     "your name please",
     "your name",
     "may i know your name",
     "what is your name",
     "what call yourself",
   ],
-  ["i love you"],
-  ["happy", "good", "fun", "wonderful", "fantastic", "cool", "very good"],
-  ["bad", "bored", "tired"],
-  ["help me", "tell me story", "tell me joke"],
-  ["ah", "ok", "okay", "nice", "welcome"],
-  ["thanks", "thank you"],
-  ["what should i eat today"],
-  ["bro"],
-  ["what", "why", "how", "where", "when"],
-  ["corona", "covid19", "coronavirus"],
-  ["you are funny"],
-  ["i dont know"],
-  ["boring"],
-  ["im tired"],
+  ["i love you"], //11
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool", "very good"], //12
+  ["bad", "bored", "tired"], //13
+  ["help me", "tell me story", "tell me joke"], //14
+  ["ah", "ok", "okay", "nice", "welcome"], //15
+  ["thanks", "thank you"], //16
+  ["what should i eat today"], //17
+  ["bro"], //18
+  ["what", "why", "how", "where", "when"], //19
+  ["corona", "covid19", "coronavirus"], //20
+  ["you are funny"], //21
+  ["i dont know"], //22
+  ["boring"], //23
+  ["im tired"], //24
   [
+    //25
     "what are your rates",
     "what is your rates",
     "rates",
@@ -43,20 +45,23 @@ export const userMessage = [
     "Hello goodmorning, can i ask about your rates",
   ],
   [
+    //26
     "what are your packages",
     "packages",
     "ano ang packages niyo",
     "package",
     "ano ang services nyo",
   ],
-  ["offers", "What are your offers"],
+  ["offers", "What are your offers"], //27
   [
-    "location",
+    //28
+    "Where is your location",
     "Where are you located?",
     "where are you?",
     "where is your place?",
   ],
   [
+    //29
     "book",
     "how to book?",
     "how do i book",
@@ -69,6 +74,7 @@ export const userMessage = [
     "how do i make a reservation?",
   ],
   [
+    //30
     "contact",
     "contacts",
     "contact us",
@@ -82,6 +88,7 @@ export const userMessage = [
     "who to contact?",
   ],
   [
+    //31
     "Activities",
     "activity",
     "what are your activities?",
@@ -92,60 +99,72 @@ export const userMessage = [
 
 export const botReply = [
   [
+    //1
     "Hello! I am Modebot you can ask me about anything in our resort like packages, location, and rates.",
     "Hi! I am Modebot you can ask me about anything in our resort like packages, location, and rates.",
     "Hey! I am Modebot you can ask me about anything in our resort like packages, location, and rates.",
     "Hi there! I am Modebot you can ask me about anything in our resort like packages, location, and rates.",
   ],
-  ["Okay"],
-  ["Yes I am! "],
-  ["I'm sorry about that. But I like you dude."],
+  ["Okay"], //2
+  ["Yes I am!"], //3
+  ["I'm sorry about that. But I like you dude."], //4
   [
+    //5
     "Fine... how are you?",
     "Pretty well, how are you?",
     "Fantastic, how are you?",
   ],
   [
+    //6
     "Nothing much",
     "About to go to sleep",
     "Can you guess?",
     "I don't know actually",
-  ],
-  ["I am always young."],
-  ["I am just a bot", "I am a bot. What are you?"],
-  ["Vince pogi"],
-  ["I am Modebot"],
-  ["I love you too", "Me too"],
-  ["Have you ever felt bad?", "Glad to hear it"],
-  ["Why?", "Why? You shouldn't!", "Try watching TV", "Chat with me."],
-  ["What about?", "Once upon a time..."],
-  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
-  ["You're welcome"],
-  ["Briyani", "Burger", "Sushi", "Pizza"],
-  ["Dude!"],
-  ["Yes?"],
-  ["Please stay home"],
-  ["Glad to hear it"],
-  ["Say something interesting"],
-  ["Sorry for that. Let's chat!"],
-  ["Take some rest, Dude!"],
-  ["Our rates are ranging from 150 to 180 pesos."],
+  ], //7
+  ["I am always young."], //8
+  ["I am just a bot", "I am a bot. What are you?"], //9
+  ["Vince pogi"], //10
+  ["I am Modebot"], //11
+  ["I love you too", "Me too"], //12
+  ["Have you ever felt bad?", "Glad to hear it"], //13
+  ["Why?", "Why? You shouldn't!", "Try watching TV", "Chat with me."], //14
+  ["What about?", "Once upon a time..."], //15
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"], //16
+  ["You're welcome"], //17
+  ["Briyani", "Burger", "Sushi", "Pizza"], //18
+  ["Dude!"], //19
+  ["Yes?"], //20
+  ["Please stay home"], //21
+  ["Glad to hear it"], //22
+  ["Say something interesting"], //23
+  ["Sorry for that. Let's chat!"], //24
+  ["Take some rest, Dude!"], //25
+  ["Our rates are ranging from 150 to 180 pesos."], //26
   [
+    //27
     "We have packages like DayTour Weekdays, DayTour Weekends & Holidays, Overnight, Day and Night for 22hrs. You can check their price lists in our Package Section.",
   ],
   [
+    //28
     "We have packages like DayTour Weekdays, DayTour Weekends & Holidays, Overnight, Day and Night for 22hrs. You can check their price lists in our Package Section.",
   ],
   [
+    //28
+    "Modesto's farm and resort is located at Sitio 2, Sahingan, Balete, Batangas City you can search us in google, waze, maps.",
+    "Modesto's farm and resort is located at Sitio 2, Sahingan, Balete, Batangas City you can search us in google, waze, maps.",
+    "Modesto's farm and resort is located at Sitio 2, Sahingan, Balete, Batangas City you can search us in google, waze, maps.",
     "Modesto's farm and resort is located at Sitio 2, Sahingan, Balete, Batangas City you can search us in google, waze, maps.",
   ],
   [
+    //30
     "You can book in our home page by clicking 'book now!'. Then fill up the forms, input the date and time when you would visit our resort then click 'done'. Your reservation would be sent to our human representative and will sent you a confirmation email.Thank you!",
   ],
   [
+    //31
     "You can contact our human representative in our facebook page-https://www.facebook.com/profile.php?id=100090637717825&mibextid=JRoKGi, or contact us in this phone number-xxxxxxxxxx. For more details contact Ma'am Rowena Aguado in her facebook-https://www.facebook.com/rowena.aguado?mibextid=JRoKGi",
   ],
   [
+    //32
     "Our activities includes swimming, billiard, table hockey, darts, and many more.",
   ],
 ];
